addac22f1633a5131ad51af2c2ec68fe7c6ceeb9
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-07-10 07:20:09 +0900

    feat :: all

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Home() {
+  const router = useRouter();
+  var level = 0;
+
+  useEffect(() => {
+    sessionStorage.setItem("level", "0");
+  }, []);
+
+  function Fild() {
+    router.push("/Fild");
+  }
+
+  function newFild() {
+    router.push("/Signup");
+  }
+  return (
+    <div className="flex items-center justify-center">
+      <div className="h-[100vh] w-[80vh] bg-[#000000] flex justify-center items-center flex-col ">
+        <p className="text-[#FFFFFF] text-[8vh] ">모험가 이야기</p>
+
+        <button className="text-[#FFFFFF] text-[5vh] transition-all duration-300 hover:text-[6vh]" onClick={() => Fild()}>이어서 하기</button>
+        <button className="text-[#FFFFFF] text-[5vh] transition-all duration-300 hover:text-[6vh]" onClick={() => newFild()}>새로 시작하기</button>
+      </div>
+    </div>
+  );
+}
